8d2d4d78bf0807b657d90e2f37bd6c48635a7875
aelael <dlagh@yandex.com>
2019-01-03 00:26:02 +0300

    Commit before refractoring

--- functional_tests.py
+++ functional_tests.py
@@ -38,21 +38,28 @@ class NewVisitorTest(unittest.TestCase):
         # After User presses ENTER key, page reloads with new To-Do List element
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
         table = self.browser.find_element_by_id('id_list_table')
         rows  = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == 'Test Item 1' for row in rows),
-            "New To-Do item did not appear in table"
-        )
+        self.assertIn('1: Test Item 1', [row.text for row in rows])
 
         # User is prompted to enter another To-Do List item
         # User enters list item "Test Item 2"
-        self.fail('Ending test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Test Item 2')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # After page updates, it shows both items in the User list
-        # User sees notice that unique URL has been generated for his To-Do List
+        table = self.browser.find_element_by_id('id_list_table')
+        rows  = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Test Item 1', [row.text for row in rows])
+        self.assertIn(
+            '2: Test Item 2',
+            [row.text for row in rows]
+        )
 
+        # User sees notice that unique URL has been generated for his To-Do List
+        self.fail('Test end!')
         # User follows the link and checks his To-Do List
 
 if __name__ == '__main__':
