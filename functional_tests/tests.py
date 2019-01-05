827ad03f6877f5a4f8be906be8c0c0b814523214
aelael <dlagh@yandex.com>
2019-01-05 22:55:02 +0300

    Swapped two lines of code :)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,8 +2,11 @@ from django.test                    import LiveServerTestCase
 
 from selenium                       import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions     import WebDriverException
 
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     '''new User test'''
@@ -16,11 +19,19 @@ class NewVisitorTest(LiveServerTestCase):
         '''tearing down'''
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        '''checking for a row in the list table'''
-        table = self.browser.find_element_by_id('id_list_table')
-        rows  = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        '''waiting for a row in the list table'''
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows  = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''test: User can start a list and retrieve it later'''
@@ -44,21 +55,17 @@ class NewVisitorTest(LiveServerTestCase):
         
         # After User presses ENTER key, page reloads with new To-Do List element
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Test Item 1')
+        self.wait_for_row_in_list_table('1: Test Item 1')
 
         # User is prompted to enter another To-Do List item
         # User enters list item "Test Item 2"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Test Item 2')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # After page updates, it shows both items in the User list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows  = table.find_elements_by_tag_name('tr')
-        self.check_for_row_in_list_table('1: Test Item 1')
-        self.check_for_row_in_list_table('2: Test Item 2')
+        self.wait_for_row_in_list_table('1: Test Item 1')
+        self.wait_for_row_in_list_table('2: Test Item 2')
 
         # User sees notice that unique URL has been generated for his To-Do List
         self.fail('Test end!')
